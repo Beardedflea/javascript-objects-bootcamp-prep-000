1e5fd29ffbf0bdf4cee746b6a948a036390cef5b
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-09-01 10:54:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,7 @@
-var playlist = new Object()
+var playlist = new Object();
+
+playlist.['Slowdive'] = 'Alison';
+playlist.['My Bloody Valentine'] = 'Sometimes';
 
 
-playlists.['Phil Ochs'] = "Here's to the State of Mississippi";
+playlists.['Phil Ochs'] = 'Here's to the State of Mississippi';
