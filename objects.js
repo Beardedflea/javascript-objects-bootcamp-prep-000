ecff439fe7192965ec22c77547a943b93befa49e
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-09-01 11:06:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -11,3 +11,10 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 updatePlaylist(playlist,'Phil Ochs', "Here's to the State of Mississippi");
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
+
+removeFromPlaylist(playlist, artistName)
